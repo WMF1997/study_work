bc1ff19c024365ff95a910dba49dd2ed8c1d46d7
王明非 <wmf997@126.com>
2021-04-11 22:21:22 +0800

    hello

--- 20210411/try_fp/CountLinesInFiles.cpp
+++ 20210411/try_fp/CountLinesInFiles.cpp
@@ -14,12 +14,23 @@ std::vector<int> CountLinesInFiles(const std::vector<std::string>& Files)
         std::ifstream In(File);
         while (In.get(c))
         {
-            
+            if (c=='\n')
+            {
+                LineCount++;
+            }
         }
+        Results.push_back(LineCount);
     }
+    return Results;
 }
 
 int main(void)
 {
-    
+    std::vector<std::string> Files = {"CountLinesInFiles.cpp"};
+    std::vector<int> Result = CountLinesInFiles(Files);
+    for (auto& OneResult : Result)
+    {
+        cout << OneResult << endl;
+    }
+    return 0;
 }
